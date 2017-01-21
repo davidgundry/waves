10f856fba4c022869bf96ec9364c21331c65b142
Harissa <contact@joecutting.com>
2017-01-21 19:00:53 +0000

    adding things and updates the inventory state

--- src/Inventory.ts
+++ src/Inventory.ts
@@ -20,11 +20,7 @@
             if (this.boundsRect.contains(x, y)) {
                 var slot: number = this.getSlot(item.position.x, item.position.y);
                     if ((this.slots[slot] === null)  || (slot === item.inventorySlot)) {
-                        this.slots[slot] = item;
-                        if (item.inventorySlot) {
-                            this.slots[item.inventorySlot] = null;
-                        }
-                        item.inventorySlot = slot;
+                        this.setItem(item, slot);
                     var centre: Phaser.Point = this.getSlotMiddle(slot);
                     this.game.add.tween(item).to({ x: centre.x, y: centre.y }, 1000, "Sine.easeIn",true);
                     return true;
@@ -32,7 +28,18 @@
             }
             return false;
         }
+        setItem(item: InventoryItem, slot: number) {
+            this.slots[slot] = item;
+            if (item.inventorySlot) {
+                this.slots[item.inventorySlot] = null;
+            } else {
+                (<Game>this.game).model.inventory.AddItem(item.baseThing);
+            }
+            item.inventorySlot = slot;
+            
+        }
         removeItem(item: InventoryItem) {
+            (<Game>this.game).model.inventory.DiscardItem(item.baseThing);
             this.slots[item.inventorySlot] = null;
             item.inventorySlot = null;
         }
