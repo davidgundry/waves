50a69c16bde5ca3125df59f52b18678542ab13f8
Harissa <contact@joecutting.com>
2017-01-21 15:02:19 +0000

    can't remember

--- app.js
+++ app.js
@@ -24,45 +24,6 @@ window.onload = function () {
 };
 var Waves;
 (function (Waves) {
-<<<<<<< HEAD
-    var Boot = (function (_super) {
-        __extends(Boot, _super);
-        function Boot() {
-            _super.apply(this, arguments);
-            this.orientated = false;
-        }
-        Boot.prototype.preload = function () {
-            _super.prototype.preload.call(this);
-            this.load.image('preloadBar', 'assets/whiteLoadBar.png');
-        };
-        Boot.prototype.create = function () {
-            _super.prototype.create.call(this);
-            this.input.maxPointers = 1;
-            this.stage.disableVisibilityChange = true;
-            if (this.game.device.desktop) {
-                this.scale.scaleMode = Phaser.ScaleManager.NO_SCALE;
-                this.scale.pageAlignHorizontally = true;
-                this.scale.pageAlignVertically = true;
-            }
-            else {
-                this.scale.fullScreenScaleMode = Phaser.ScaleManager.NO_SCALE;
-                this.scale.pageAlignHorizontally = true;
-                this.scale.pageAlignVertically = true;
-                this.scale.refresh();
-                var gameElement = document.getElementById('game');
-                gameElement.style.overflow = "visible";
-            }
-            this.game.state.start('Preloader', true, false);
-        };
-        return Boot;
-    })(Phaser.State);
-    Waves.Boot = Boot;
-    ;
-})(Waves || (Waves = {}));
-var Waves;
-(function (Waves) {
-=======
->>>>>>> 2fcec0cf72895cada2a59b3dac465f86fb14fbec
     var Button = (function (_super) {
         __extends(Button, _super);
         function Button(game, label) {
@@ -109,24 +70,6 @@ var Waves;
             this.position.y = newY;
             this.setDrag(true);
         }
-<<<<<<< HEAD
-        MainGame.prototype.create = function () {
-            _super.prototype.create.call(this);
-            this.mainButton = new Waves.Button(this.game, "Paddle with your hands");
-            this.mainButton.pressed.add(this.onPress);
-            this.milesDisplay = this.game.add.text(300, 10, "Testing 12 12", { font: "30px Arial", fill: '#00f', align: 'right' });
-            this.updateMiles();
-        };
-        MainGame.prototype.onPress = function () {
-            //alert("pressed");
-            this.game.model.world.MoveDistance(1);
-            this.updateMiles();
-        };
-        MainGame.prototype.updateMiles = function () {
-            alert("Miles " + this.game.model.world.milesRemaining);
-            //this.milesDisplay.text = "You are " + (<Game>this.game).model.world.milesRemaining + " miles from land";
-            this.milesDisplay.text = "You are " + 5 + " miles from land";
-=======
         InventoryItem.prototype.setDrag = function (isEnabled) {
             this.baseSprite.inputEnabled = isEnabled;
             if (isEnabled) {
@@ -139,7 +82,6 @@ var Waves;
             }
         };
         InventoryItem.prototype.onDragStart = function () {
->>>>>>> 2fcec0cf72895cada2a59b3dac465f86fb14fbec
         };
         InventoryItem.prototype.onDragStop = function () {
             this.position.x = this.baseSprite.position.x;
@@ -147,14 +89,6 @@ var Waves;
             this.baseSprite.position.x = 0;
             this.baseSprite.position.y = 0;
         };
-<<<<<<< HEAD
-        return MainGame;
-    })(Phaser.State);
-    Waves.MainGame = MainGame;
-})(Waves || (Waves = {}));
-var Waves;
-(function (Waves) {
-=======
         return InventoryItem;
     }(Phaser.Group));
     Waves.InventoryItem = InventoryItem;
@@ -173,7 +107,6 @@ var Waves;
 })(Waves || (Waves = {}));
 var Waves;
 (function (Waves) {
->>>>>>> 2fcec0cf72895cada2a59b3dac465f86fb14fbec
     var InventoryState = (function () {
         function InventoryState() {
             this._totalSpace = InventoryState.STARTING_TOTAL_SPACE;
@@ -235,6 +168,40 @@ var Waves;
 })(Waves || (Waves = {}));
 var Waves;
 (function (Waves) {
+    var ThingPosition = (function () {
+        function ThingPosition(thing, distance) {
+            this.thing = thing;
+            this.distance = distance;
+        }
+        Object.defineProperty(ThingPosition.prototype, "thing", {
+            get: function () {
+                return this._thing;
+            },
+            enumerable: true,
+            configurable: true
+        });
+        Object.defineProperty(ThingPosition.prototype, "distance", {
+            get: function () {
+                return this._distance;
+            },
+            enumerable: true,
+            configurable: true
+        });
+        return ThingPosition;
+    }());
+    Waves.ThingPosition = ThingPosition;
+})(Waves || (Waves = {}));
+var Waves;
+(function (Waves) {
+    var Trigger = (function () {
+        function Trigger() {
+        }
+        return Trigger;
+    }());
+    Waves.Trigger = Trigger;
+})(Waves || (Waves = {}));
+var Waves;
+(function (Waves) {
     var WorldState = (function () {
         function WorldState() {
             this._milesRemaining = WorldState.STARTING_MILES;
@@ -268,12 +235,6 @@ var Waves;
         return WorldState;
     }());
     Waves.WorldState = WorldState;
-<<<<<<< HEAD
-=======
-    ThingsInView();
-    ThingPosition;
-    {
-    }
 })(Waves || (Waves = {}));
 var Waves;
 (function (Waves) {
@@ -340,7 +301,6 @@ var Waves;
         return MainGame;
     }(Phaser.State));
     Waves.MainGame = MainGame;
->>>>>>> 2fcec0cf72895cada2a59b3dac465f86fb14fbec
 })(Waves || (Waves = {}));
 var Waves;
 (function (Waves) {
